dee7c2e78e047934e75c40f1370d2e846e4878df
Bella <bella.who.two@gmail.com>
2020-01-17 11:01:42 -0500

    fix it lmaoo xd

--- src/main/java/me/zeroeightsix/kami/module/modules/bewwawho/combat/AutoExp.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/bewwawho/combat/AutoExp.java
@@ -22,8 +22,8 @@ public class AutoExp extends Module {
     private Setting<Boolean> autoThrow = register(Settings.b("Auto Throw", true));
     private Setting<Boolean> autoSwitch = register(Settings.b("Auto Switch", true));
     private Setting<Boolean> autoDisable = register(Settings.booleanBuilder("Auto Disable").withValue(true).withVisibility(o -> autoSwitch.getValue()).build());
-    private Setting<Boolean> checkRepairable = register(Settings.b("Check Repairable", true));
     private Setting<Boolean> mendOffhand = register(Settings.b("Mend Offhand", false));
+    private Setting<Boolean> checkRepairable = register(Settings.b("Auto Repair", true));
     private Setting<Integer> threshold = register(Settings.integerBuilder("Repair %").withMinimum(1).withMaximum(100).withValue(70));
     private Setting<Integer> delay = register(Settings.integerBuilder("Delay (Ticks)").withMinimum(1).withValue(1));
 
@@ -60,7 +60,52 @@ public class AutoExp extends Module {
 
     }
 
-    // /give @p diamond_chestplate 1 527 {ench:[{lvl:1s,id:70s}]}
+    @Override
+    public void onUpdate() {
+        if (mc.player == null) return;
+
+        if (slotNotEmpty(1)) Command.sendChatMessage("not empty");
+        if (delay.getValue() != oldDelay) usableDelay = delay.getValue();
+
+        oldDelay = delay.getValue();
+
+        if (usableDelay > 0) {
+            usableDelay--;
+            return;
+        } else {
+            usableDelay = delay.getValue();
+        }
+
+        if (checkRepairable.getValue()
+                && ((slotNotEmpty(0) && canMendArmour(0) && shouldMendArmour(0))
+                || (slotNotEmpty(1) && canMendArmour(1) && shouldMendArmour(1))
+                || (slotNotEmpty(2) && canMendArmour(2) && shouldMendArmour(2))
+                || (slotNotEmpty(3) && canMendArmour(3) && shouldMendArmour(3))
+                || (offhandNotEmpty() && canMendOffhand() && shouldMendOffhand()))) {
+            return;
+        }
+
+        if (autoSwitch.getValue() && (mc.player.getHeldItemMainhand().getItem() != Items.EXPERIENCE_BOTTLE)) {
+            int xpSlot = findXpPots();
+            if (xpSlot == -1) {
+                if (autoDisable.getValue()) {
+                    Command.sendWarningMessage("[AutoExp] No XP in hotbar, disabling");
+                    this.disable();
+                }
+                return;
+            }
+            mc.player.inventory.currentItem = xpSlot;
+        }
+
+        if (autoThrow.getValue() && mc.player.getHeldItemMainhand().getItem() == Items.EXPERIENCE_BOTTLE) {
+            mc.rightClickMouse();
+        }
+    }
+
+    /*
+/give @p diamond_chestplate 1 200 {ench:[{lvl:1s,id:70s}]}
+    */
+
     /* Should do mending and can mend */
     private boolean shouldMendArmour(int i) { // (100 * damage / max damage) >= (100 - 70)
         return (
@@ -100,46 +145,5 @@ public class AutoExp extends Module {
             }
         }
         return slot;
-    }
-
-    @Override
-    public void onUpdate() {
-        if (mc.player != null) return;
-
-        if (delay.getValue() != oldDelay) usableDelay = delay.getValue();
-
-        oldDelay = delay.getValue();
-
-        if (usableDelay > 0) {
-            usableDelay--;
-            return;
-        } else {
-            usableDelay = delay.getValue();
-        }
-
-        if (checkRepairable.getValue()
-                && !((slotNotEmpty(0) && canMendArmour(0) && shouldMendArmour(0))
-                || (slotNotEmpty(1) && canMendArmour(1) && shouldMendArmour(1))
-                || (slotNotEmpty(2) && canMendArmour(2) && shouldMendArmour(2))
-                || (slotNotEmpty(3) && canMendArmour(3) && shouldMendArmour(3))
-                || (offhandNotEmpty() && canMendOffhand() && shouldMendOffhand()))) {
-            return;
-        }
-
-        if (autoSwitch.getValue() && (mc.player.getHeldItemMainhand().getItem() != Items.EXPERIENCE_BOTTLE)) {
-            int xpSlot = findXpPots();
-            if (xpSlot == -1) {
-                if (autoDisable.getValue()) {
-                    Command.sendWarningMessage("[AutoExp] No XP in hotbar, disabling");
-                    this.disable();
-                }
-                return;
-            }
-            mc.player.inventory.currentItem = xpSlot;
-        }
-
-        if (autoThrow.getValue() && mc.player.getHeldItemMainhand().getItem() == Items.EXPERIENCE_BOTTLE) {
-            mc.rightClickMouse();
-        }
     }
 }
